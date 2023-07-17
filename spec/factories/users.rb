f0aec56a43e2a32d716e006bf2fb9086beb3e019
lemonade-1 <m.of.mountain@gmail.com>
2023-07-18 06:08:51 +0900

    userモデル単体テスト

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
+    password_confirmation {password}
+    last_name { 'てすと' }
+    first_name { '太郎' }
+    last_name_kana { 'テスト' }
+    first_name_kana { 'タロウ' }
+    birthday { Faker::Date.birthday }
   end
 end
