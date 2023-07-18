cfd0e7a3f7997ad68da012f52f5df3f76edd5e9a
lemonade-1 <m.of.mountain@gmail.com>
2023-07-18 19:28:55 +0900

    新規登録・ログイン時のエラーハンドリングの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX , message: 'Include both letters and numbers' }
+  validates :password, format: { with: VALID_PASSWORD_REGEX , message: 'Include both letters and numbers', allow_blank: true }
 
   validates :nickname,        presence: true
-  validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }, presence: true
-  validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }, presence: true
-  validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ },      presence: true
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ },      presence: true
+  validates :last_name,       format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name,      format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/ }     
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }     
   validates :birthday,        presence: true
 
 end
