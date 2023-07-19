af72f1530018aedb1a595339a3606bde4c1ddb6d
lemonade-1 <m.of.mountain@gmail.com>
2023-07-20 01:25:30 +0900

    ActiveHashのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,31 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
+
+  #空の投稿を保存できないようにする
+  with_options presence: true do
+    validates :user_id
+    validates :name
+    validates :description
+    validates :category_id 
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  with_options numericality: { other_than: 1 , message: "can't be blank" } do
+    validates :category_id 
+    validates :condition_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,12 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'Include both letters and numbers', allow_blank: true }
 
-  validates :nickname,        presence: true
-  validates :last_name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, presence: true }
-  validates :first_name,      format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, presence: true }
-  validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, presence: true }
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, presence: true }
-  validates :birthday,        presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :last_name,       format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }
+    validates :first_name,      format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true }
+    validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, allow_blank: true }
+    validates :birthday
+  end
 end
