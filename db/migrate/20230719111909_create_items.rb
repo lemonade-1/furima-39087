b293cdd20cc50112dff745e97a6395549f108dfe
lemonade-1 <m.of.mountain@gmail.com>
2023-07-19 20:48:17 +0900

    Itemモデルの作成

---
+++ db/migrate/20230719111909_create_items.rb
@@ -0,0 +1,10 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :name, null: false
+      t.text :description, null: false
+      t.timestamps
+    end
+  end
+end
