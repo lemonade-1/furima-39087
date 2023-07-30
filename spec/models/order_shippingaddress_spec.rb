cf1b22902e2d021538c7b4a99d6ea1dd94c9df62
lemonade-1 <m.of.mountain@gmail.com>
2023-07-30 22:13:31 +0900

    【修正】テストコード

--- spec/models/order_shippingaddress_spec.rb
+++ spec/models/order_shippingaddress_spec.rb
@@ -2,7 +2,9 @@ require 'rails_helper'
 
 RSpec.describe OrderShippingaddress, type: :model do
   before do
-    @order_shippingaddress = FactoryBot.build(:order_shippingaddress)
+    @user = FactoryBot.create(:user)
+    @item = FactoryBot.create(:item)
+    @order_shippingaddress = FactoryBot.build(:order_shippingaddress, user_id: @user.id, item_id: @item.id)
   end
 
   describe '配送先住所保存' do
